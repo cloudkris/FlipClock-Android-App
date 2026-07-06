2a3ea5405d469a2a273291bfd0b5ea0657eda3be
cloudkris <69116063+cloudkris@users.noreply.github.com>
2026-07-06 17:45:35 +1000

    Update MainActivity.kt

--- app/src/main/java/com/cloudkris/flipclock/MainActivity.kt
+++ app/src/main/java/com/cloudkris/flipclock/MainActivity.kt
@@ -28,15 +28,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.CornerRadius
+
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
+
 import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.clipPath
+
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.TextStyle
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
